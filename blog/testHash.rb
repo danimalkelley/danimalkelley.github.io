844ca0975edccf4518d8c43f5e2225ee4d87b866
danimalkelley <daniel.congdon.kelley@gmail.com>
2014-10-25 15:55:26 -0700

    add stuff to t3 blog

---
+++ blog/testHash.rb
@@ -0,0 +1,8 @@
+simple_hash = {
+            'USA' => 'North America',
+            'Colombia' => 'South America',
+            'Cambodia' => 'Asia',
+            'France' => 'Europe'
+          }
+
+puts simple_hash['USA']
