7644a3d6e8e40bf4f22dd585df8491f6209f41e6
danimalkelley <daniel.congdon.kelley@gmail.com>
2014-10-25 21:13:15 -0700

    add blog post teasers and reformat blog posts

--- blog/testHash.rb
+++
@@ -1,8 +0,0 @@
-simple_hash = {
-            'USA' => 'North America',
-            'Colombia' => 'South America',
-            'Cambodia' => 'Asia',
-            'France' => 'Europe'
-          }
-
-puts simple_hash['USA']
